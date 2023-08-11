4bc7843d6c45ffc632ec41e5b4ce4ce722169d1b
rozhur <rozhur@zhdev.org>
2023-08-11 19:42:02 +0500

    Create velocity module classes

---
+++ velocity/src/main/java/org/zhdev/varioutil/VelocityPlugin.java
@@ -0,0 +1,30 @@
+package org.zhdev.varioutil;
+
+import com.velocitypowered.api.proxy.ProxyServer;
+import org.slf4j.Logger;
+
+import java.nio.file.Path;
+
+public abstract class VelocityPlugin {
+    protected final ProxyServer server;
+    protected final Logger logger;
+    protected final Path dataFolder;
+
+    protected VelocityPlugin(ProxyServer server, Logger logger, Path dataDirectory) {
+        this.server = server;
+        this.logger = logger;
+        this.dataFolder = dataDirectory;
+    }
+
+    public ProxyServer getServer() {
+        return server;
+    }
+
+    public Path getDataDirectory() {
+        return dataFolder;
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+}
---
+++ velocity/src/main/java/org/zhdev/varioutil/config/VelocityTomlConfig.java
@@ -0,0 +1,128 @@
+package org.zhdev.varioutil.config;
+
+import com.electronwill.nightconfig.core.CommentedConfig;
+import com.electronwill.nightconfig.toml.TomlFormat;
+import com.electronwill.nightconfig.toml.TomlParser;
+import com.electronwill.nightconfig.toml.TomlWriter;
+
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Path;
+import java.util.Map;
+
+public final class VelocityTomlConfig extends MapConfigSection implements Config {
+    private static final String DEFAULT_KEY = "config.toml";
+
+    private final String key;
+
+    public VelocityTomlConfig(String key) {
+        super();
+        this.key = key;
+    }
+
+    public VelocityTomlConfig() {
+        this(DEFAULT_KEY);
+    }
+
+    @Override
+    public String getKey() {
+        return key;
+    }
+
+    private String[] splitComments(String comment) {
+        String[] comments = comment.split("\n");
+        for (int i = 0; i < comments.length; i++) {
+            comments[i] = comments[i].trim();
+        }
+        return comments;
+    }
+
+    private String joinComments(String[] comments) {
+        return ' ' + String.join("\n ", comments);
+    }
+
+    private void nightConfigToSections(MapConfigSection section, CommentedConfig config) {
+        for (CommentedConfig.Entry entry : config.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+
+            if (value instanceof CommentedConfig) {
+                MapConfigSection childSection;
+                ConfigSectionNode node  = section.map.get(key);
+                if (node == null || !(node.value instanceof MapConfigSection)) {
+                    childSection = new MapConfigSection();
+                } else {
+                    childSection = (MapConfigSection) node.value;
+                }
+                nightConfigToSections(childSection, (CommentedConfig) value);
+                value = childSection;
+            }
+
+            ConfigSectionNode sectionNode = new ConfigSectionNode(value);
+
+            if (entry.getComment() != null) {
+                sectionNode.blockComments = splitComments(entry.getComment());
+            }
+
+            section.map.put(key, sectionNode);
+        }
+    }
+
+    @Override
+    public void load(Reader reader) {
+        TomlParser tomlParser = new TomlParser();
+        CommentedConfig commentedConfig = tomlParser.parse(reader);
+        nightConfigToSections(this, commentedConfig);
+    }
+
+    @Override
+    public Path load() throws ConfigException {
+        return load(key);
+    }
+
+    private void sectionsToNightConfig(MapConfigSection section, CommentedConfig config) {
+        for (Map.Entry<String, ConfigSectionNode> entry : section.map.entrySet()) {
+            String key = entry.getKey();
+            ConfigSectionNode sectionNode = entry.getValue();
+            Object value = sectionNode.value;
+            if (value instanceof MapConfigSection) {
+                CommentedConfig subConfig = config.createSubConfig();
+                sectionsToNightConfig((MapConfigSection) value, subConfig);
+                value = subConfig;
+            }
+
+            config.set(key, value);
+
+            if (sectionNode.blockComments != null) {
+                config.setComment(key, joinComments(sectionNode.blockComments));
+            }
+
+            if (sectionNode.inlineComments != null) {
+                config.setComment(key, config.getComment(key) + "\n\n" + joinComments(sectionNode.inlineComments));
+            }
+        }
+    }
+
+    @Override
+    public void save(Writer writer) {
+        TomlWriter tomlWriter = new TomlWriter();
+        CommentedConfig config = TomlFormat.newConfig();
+        sectionsToNightConfig(this, config);
+        tomlWriter.write(config, writer);
+    }
+
+    @Override
+    public Path save() throws ConfigException {
+        return save(key);
+    }
+
+    @Override
+    public Path saveIfEmpty() throws ConfigException {
+        return saveIfEmpty(key);
+    }
+
+    @Override
+    public String toString() {
+        return key;
+    }
+}
